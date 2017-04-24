451724dd48ab381b392a9268c2aea1dc5832b22e
Dieter Menne <dieter.menne@menne-biomed.de>
2017-04-24 16:01:12 +0200

    Corrected test results

--- tests/testthat/test_stan_fit.R
+++ tests/testthat/test_stan_fit.R
@@ -30,13 +30,13 @@ test_that("Data that cannot be fitted with nls_list/nlme work with stan_fit", {
   expect_identical(unique(cf$group), "A")
   expect_identical(unique(cf$parameter), c("beta", "k", "m", "t50", "tlag"))
   expect_identical(unique(cf$stat), c("estimate", "q_0275", "q_25", "q_75", "q_975"))
-  expect_equal(nrow(cf), 400)
+  expect_equal(nrow(cf), 395)
   expect_equal(ncol(cf), 6)
 
   cf = coef(fit) # This is the "mean" group only
   expect_identical(unique(cf$group), "A")
   expect_identical(unique(cf$parameter), c("beta", "k", "m", "t50", "tlag"))
-  expect_equal(nrow(cf), 80)
+  expect_equal(nrow(cf), 79)
   expect_equal(ncol(cf), 5)
 })
 
@@ -60,7 +60,7 @@ test_that("Multiple chains return valid results similar to nlme", {
   chains = 2
   student_t_df = 10
   dose = 100
-  iter = 100
+  iter = 500
   sample_minutes = 15
   data = cleanup_data(simulate_breathtest_data(n_records = 1, seed = 100)$data)
   fit = stan_fit(data, dose = dose, student_t_df = student_t_df,
@@ -74,19 +74,18 @@ test_that("Multiple chains return valid results similar to nlme", {
     summarize(
       rel_diff = mean(rel_diff)
     )
-  expect_lt(cf$rel_diff, 0.03)
+  expect_lt(cf$rel_diff, 0.001)
 })
 
-test_that("Non-gaussian residuals with student_t_df work with outliers", {
+test_that("Non-gaussian residuals with student_t_df <10 gives result close to nlme", {
   skip_on_cran()
-
   library(breathtestcore)
   chains = 1
   student_t_df = 3
   dose = 100
-  iter = 100
+  iter = 500
   sample_minutes = 15
-  data = cleanup_data(simulate_breathtest_data(n_records = 1, seed = 100,
+  data = cleanup_data(simulate_breathtest_data(n_records = 1, seed = 1000,
                                          student_t_df = student_t_df)$data)
   fit = stan_fit(data, dose = dose, student_t_df = student_t_df,
                  chains = chains, iter = iter  )
@@ -99,6 +98,6 @@ test_that("Non-gaussian residuals with student_t_df work with outliers", {
     summarize(
       rel_diff = mean(rel_diff)
     )
-  expect_lt(cf$rel_diff, 0.05)
+  expect_lt(cf$rel_diff, 0.04)
 })
 
