7717ab6603b0e86abf535faf9d0b98ede5afd325
Dieter Menne <dieter.menne@menne-biomed.de>
2017-07-20 19:14:14 +0200

    Reactivated all tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
 library(breathteststan)
 
-test_check("breathteststan", filter = "stan_group_fit")
+#test_check("breathteststan", filter = "stan_group_fit")
+test_check("breathteststan")
