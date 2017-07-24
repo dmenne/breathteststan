08124ad3ceff67b83e0a77f3c7cc8a10437539ef
Dieter Menne <dieter.menne@menne-biomed.de>
2017-07-24 20:30:28 +0200

    Made example dontrun

--- tests/testthat/test_coef_diff_by_group.R
+++ tests/testthat/test_coef_diff_by_group.R
@@ -20,7 +20,7 @@ test_that("Credible intervals are returned as coef_diff_by_group coefficients",{
                                      "estimate", "cred.low", "cred.high"))
   chain = attr(cf_diff, "chain")
   expect_is(chain, "data.frame")
-  expect_identical(nrow(chain), iter*chains*20)
+  expect_identical(nrow(chain), iter*chains*12)
   expect_identical(names(chain),
           c("key","value1","value2","group1","group2","value"))
 })
