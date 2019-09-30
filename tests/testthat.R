efbebfcd0cc89b228e188b88f166aff2585744b3
Dieter Menne <dieter.menne@menne-biomed.de>
2019-09-30 15:59:39 +0200

    Corrected typo rstanarm->breathteststan in travis.yml

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
 library(testthat)
 library(breathteststan)
-#options(warn = 2)
+options(warn = 2)
 # Only one test per file to avoid hanging 32-bit compile
 #test_check("breathteststan", filter = "stan_fit")
 Sys.unsetenv("R_TESTS") # https://github.com/r-lib/testthat/issues/603
