c00f74c5807b253f492dfe8ab3f88fc209b59dcd
dmenne <dmenne@users.noreply.github.com>
2022-05-09 07:21:14 +0000

    Built site for breathteststan: 0.8.5@a05508f

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(breathteststan)
-Sys.unsetenv("R_TESTS") # https://github.com/r-lib/testthat/issues/603
-test_check("breathteststan")
--- tests/testthat/test_stan_fit_1.R
+++
@@ -1,6 +0,0 @@
-test_that("stanmodels exist", {
-  expect_is(breathteststan:::stanmodels,"list")
-  expect_is(breathteststan:::stanmodels$breath_test_1, "stanmodel")
-  expect_gte(length(breathteststan:::stanmodels), 1L)
-})
-
