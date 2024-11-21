575f0ef92f8969ac3ebedcd4120653044f8fa3b4
Dieter Menne <dieter.menne@menne-biomed.de>
2024-11-21 10:37:11 +0100

    Tested in ubuntu

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,6 @@ Sys.unsetenv("R_TESTS") # https://github.com/r-lib/testthat/issues/603
 
 #options(Ncpus = 1)
 options(lifecycle_verbosity = "warning")
-options(Ncpus = parallelly::availableCores(omit = 1))
+#options(Ncpus = parallelly::availableCores(omit = 1))
 
 test_check("breathteststan")
